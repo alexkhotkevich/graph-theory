adb3b71680b299f1173add7092c94bb6bca71afc
Alexander <alexanderkhotkevich@gmail.com>
2021-08-29 19:58:36 +0300

    add firebase

--- src/App.tsx
+++ src/App.tsx
@@ -4,21 +4,27 @@ import './App.css'
 
 export function App() {
   const graph = new Graph({
-    directed: true,
-    vertices: new Set<Vertex>([0, 1, 2, 3, 4, 5, 7]),
+    directed: false,
+    vertices: new Set<Vertex>([0, 1, 2, 3, 4]),
     edges: new Set<Edge>([
-      { start: 0, end: 1, weight: 2 },
-      { start: 0, end: 2, weight: 4 },
-      { start: 1, end: 2, weight: 6 },
-      { start: 2, end: 3, weight: 8 },
-      { start: 3, end: 4 },
+      { start: 0, end: 3, weight: 1 },
+      { start: 3, end: 1, weight: 1 },
+      { start: 1, end: 4, weight: 1 },
+      { start: 4, end: 2, weight: 1 },
+      { start: 2, end: 0 },
     ]),
   })
 
   const step = (2 * Math.PI) / graph.vertices.size
 
   return (
-    <div className="root">
+    <div className="row">
+      <div className="row">
+        <div className="column">
+          <h3>Adjacency Matrix</h3>
+          <pre style={{ fontSize: '1.125rem' }}>{graph.adjacencyMatrix.join('\n').replaceAll(',', ' ')}</pre>
+        </div>
+      </div>
       <svg height="100vmin" width="100vmin">
         <defs>
           <marker id="arrowhead" markerWidth="5" markerHeight="3" refX="5" refY="1.5" orient="auto">
@@ -27,17 +33,21 @@ export function App() {
         </defs>
         {[...graph.adjacencyList].map((vertex, i) => (
           <React.Fragment key={i}>
-            <circle cx={40 * Math.cos(step * i) + 50 + '%'} cy={40 * Math.sin(step * i) + 50 + '%'} r="16" />
+            <circle
+              cx={40 * Math.cos(Math.PI / 10 + step * i) + 50 + '%'}
+              cy={40 * Math.sin(Math.PI / 10 + step * i) + 50 + '%'}
+              r="16"
+            />
             {vertex.map((edge, j) => (
               <line
                 key={`${i}${j}`}
-                x1={40 * Math.cos(step * i) + 50 + '%'}
-                y1={40 * Math.sin(step * i) + 50 + '%'}
-                x2={40 * Math.cos(step * edge.vertex) + 50 + '%'}
-                y2={40 * Math.sin(step * edge.vertex) + 50 + '%'}
+                x1={40 * Math.cos(Math.PI / 10 + step * i) + 50 + '%'}
+                y1={40 * Math.sin(Math.PI / 10 + step * i) + 50 + '%'}
+                x2={40 * Math.cos(Math.PI / 10 + step * edge.vertex) + 50 + '%'}
+                y2={40 * Math.sin(Math.PI / 10 + step * edge.vertex) + 50 + '%'}
                 stroke="orange"
                 strokeWidth="5"
-                markerEnd="url(#arrowhead)"
+                markerEnd={graph.directed ? 'url(#arrowhead)' : ''}
               />
             ))}
           </React.Fragment>
